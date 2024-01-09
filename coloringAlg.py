7622ffad236dcef6cddb2833996859450fdaf360
simShig <simshigol@gmail.com>
2024-01-09 20:20:38 +0200

    coloringAlg.py created and basic infrastructure

---
+++ coloringAlg.py
@@ -0,0 +1,25 @@
+from Point import Point
+
+maxCol = 0
+points = []
+def coloringAlg (value):
+    '''
+    students' algorithm for "online coloring"
+    :param value: numeric value of the point (x-coordinate)
+    :return: color_num
+    '''
+    p = Point(value)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    #your ALG here...
+
+
+    color_num = maxCol
+
+
+    #~~~~~~~~~~~~~~~~~~~~
+
+    p.col_num=color_num
+    points.append(p)
+    return color_num
